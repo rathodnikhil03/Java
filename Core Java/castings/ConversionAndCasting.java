a0235fc7039b64174c81776db021d2e72a956227
rathod-nikhil <rathodnikhil130603@gmail.com>
2024-09-07 15:07:03 +0530

    Castings Folder of core java code with Casting like ConversionAndCasting .java files

---
+++ Core Java/castings/ConversionAndCasting.java
@@ -0,0 +1,21 @@
+package castings;
+public class ConversionAndCasting {
+    
+        public static void main(String []arg){
+    
+              //conversion   
+              //if value is not valid the number is divied and reminder is 1.
+              byte a = 127;
+              int b = a;
+    
+              //casting
+              float fl = 103.12f;
+              int d = (int)fl;
+    
+             System.out.println(b);
+             System.out.println(d);
+        }
+        
+    }
+    
+
