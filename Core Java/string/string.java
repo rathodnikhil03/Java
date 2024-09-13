963eae8213d3b5109fe36bca05094aca8f72a33d
rathod-nikhil <rathodnikhil130603@gmail.com>
2024-09-13 23:56:59 +0530

    String Folder of core java code with Strings like stringbufferandbuild .java files

---
+++ Core Java/string/string.java
@@ -0,0 +1,31 @@
+public class string {
+  
+      public static void main(String[] args) {
+        
+        String name = "hello world";
+       // name = name + " Rathod";
+
+        // String name = new String("nikhil");
+
+
+        // int num = 5;
+        // Integer val = num; //auto-boxing
+        // Integer a = new Integer(num); //boxing
+
+        // int num1 = val; // auto-unboxing
+        // int num2 = val.intValue(); //unboxing
+        
+
+        // String val = "12";
+        // int num2 = Integer.parseInt(val);
+        
+        System.out.println(name.substring(6));
+        // System.out.println(num2);
+
+
+        // String s1 = "Nikhil";
+        // String s2 = "Nikhil";
+        // System.out.println(s1 == s2);
+      }
+     
+}
---
+++ Core Java/string/stringbufferandbuilder.java
@@ -0,0 +1,18 @@
+public class stringbufferandbuilder {
+
+      public static void main(String[] args) {
+        
+        StringBuffer sb = new StringBuffer("nikhil");
+        //sb.append(" rathod");
+        //sb.capacity();
+        //sb.deleteCharAt(2);
+        //sb.ensureCapacity(20);
+        //sb.insert(2, "k");
+        //String f = sb.toString();
+
+        System.out.println(sb.capacity());
+       // System.out.println(f);
+      }
+
+    
+}
