aa19ea9a5d0a7e8d5fd5e342ba2eff056b8d12b0
rathod-nikhil <rathodnikhil130603@gmail.com>
2024-09-28 23:16:54 +0530

    Adding a thoery for Lambada Exception in interfaces folder

--- Advanced Java/interfaces/LambadaEXP.java
+++ Advanced Java/interfaces/LambadaEXP.java
@@ -1,3 +1,10 @@
+//Lambda Expressions and Exceptions in Java :-
+//Lambda expressions are a concise way to represent single-method interfaces in Java. 
+//However, when working with lambda expressions, it's essential to handle exceptions properly to avoid runtime errors.
+
+//Unchecked Exceptions :-
+//Lambda expressions can throw unchecked exceptions, which are subclasses of RuntimeException.
+// These exceptions are not checked at compile-time, and they can be thrown by lambda expressions without declaring them in the throws clause.
 package interfaces;
 //@Funcationalinterface
 interface Television
