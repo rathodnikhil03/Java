a933d290743d77bb5009c450449e91b821f48e16
rathod-nikhil <rathodnikhil130603@gmail.com>
2024-10-09 21:56:26 +0530

    Adding a file in collection folder it is a LearnMap.java file

---
+++ Advanced Java/collection/LearnMap.java
@@ -0,0 +1,37 @@
+package collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LearnMap {
+   
+
+ 
+    public static void main(String[] args) {
+        // Creating a HashMap
+        Map<String, Integer> map = new HashMap<>();
+
+        // Adding key-value pairs to the HashMap
+        map.put("Apple", 10);
+        map.put("Banana", 20);
+        map.put("Cherry", 30);
+
+        
+
+        // Accessing a value using its key
+        int value = map.get("Banana");
+        System.out.println("Value for key 'Banana': " + value);
+
+        // Iterating over the key-value pairs
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+
+        // Removing a key-value pair
+        map.remove("Cherry");
+
+        // Displaying the HashMap after removal
+        System.out.println("HashMap: " + map);
+    }
+}
+
+
