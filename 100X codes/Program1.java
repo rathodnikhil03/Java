6a6817f03e94b9d16c948cb1e1b56369d3743fff
Nikhil-Rathod03 <rathodnikhil130603@gmail.com>
2025-01-21 23:48:43 +0530

    Program1 for java Challengers is 'Check if a number is even or odd.'

---
+++ 100X codes/Program1.java
@@ -0,0 +1,13 @@
+public class Program1{
+
+    public static void main(String[] args) {
+        
+        int i = 3 ;
+
+        if(i%2 == 0){
+            System.out.println("no is even" );
+        }else if(i%2 != 0){
+            System.out.println("no is odd" );
+        }
+    }
+}
