52e6fdd1e74f407ee0194bc5a74f7f3b9e6483c6
Nikhil-Rathod03 <rathodnikhil130603@gmail.com>
2025-02-08 23:52:20 +0530

    Adding the concept of Spring Boot demo project like springbootdemo

---
+++ Spring Boot/springbootdemo/src/main/java/com/study/project/Laptop.java
@@ -0,0 +1,12 @@
+package com.study.project;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+	public void complier() {
+		System.out.print("coding..");
+		
+		}
+}
