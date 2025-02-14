7749090924412282b82fb0aa82493f0d0336ac74
Nikhil-Rathod03 <rathodnikhil130603@gmail.com>
2025-02-15 00:00:21 +0530

    adding a concept of Spring Boot Web

--- Spring Boot Web/ServletEx/src/main/java/com/studey/ServletEx/App.java
+++ Spring Boot Web/ServletEx/src/main/java/com/studey/ServletEx/App.java
@@ -1,10 +1,26 @@
 package com.studey.ServletEx;
 
+import javax.servlet.Servlet;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LifecycleException {
         System.out.println("Hello World!");
+        Tomcat tomcat= new Tomcat();
+       
+        
+        Context context = tomcat.addContext("", null);
+        Tomcat.addServlet(context, "Hello", (Servlet) new HelloServlets());
+        context.addServletMappingDecoded("/hello", "Hello");
+        
+        tomcat.start();
+        tomcat.getServer().await();
+        
     }
 }
--- Spring Boot Web/ServletEx/src/main/java/com/studey/ServletEx/HelloServlets.java
+++ Spring Boot Web/ServletEx/src/main/java/com/studey/ServletEx/HelloServlets.java
@@ -1,12 +1,16 @@
 package com.studey.ServletEx;
 
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
 
 public class HelloServlets extends HttpServlet{
 
 
-	public void service() {
-		 
+	public void service(HttpServletRequest req , HttpServletResponse res){
+		 System.out.println("in service");
 		 
 	 }
 }
