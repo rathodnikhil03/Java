c4ef95caf32bc8866706ad766e9908de28ae021f
Rathod Nikhil <163429097+rathodnikhil03@users.noreply.github.com>
2025-02-18 00:02:37 +0530

    Delete Spring JDBC/SpringJDBCExample directory

--- Spring JDBC/SpringJDBCExample/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into student(rollno,name,marks) values (101,'nikhil',78);
-insert into student(rollno,name,marks) values (102,'sahil',74);
-insert into student(rollno,name,marks) values (103,'ankit',68);
