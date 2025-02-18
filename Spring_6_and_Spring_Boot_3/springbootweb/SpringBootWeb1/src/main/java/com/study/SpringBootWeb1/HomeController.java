82c286c28acacc622dbcdfb2df18d3076f0e246c
Nikhil-Rathod03 <rathodnikhil130603@gmail.com>
2025-02-18 23:49:00 +0530

    Spring Boot Web having a concept of Creating a Spring Boot Web App Project, Creating a JSP Page, Creating a Controller, RequestMapping, Sending data to Controller, Accepting Data the servlet way, Display Data on Result Page, RequestParam, Model Object, Setting Prefix and Suffix, ModelAndView,  Need for ModelAttribute, Using ModelAttribute this all thinks in SpringBootWeb1 project

--- Spring_6_and_Spring_Boot_3/springbootweb/SpringBootWeb1/src/main/java/com/study/SpringBootWeb1/HomeController.java
+++ Spring_6_and_Spring_Boot_3/springbootweb/SpringBootWeb1/src/main/java/com/study/SpringBootWeb1/HomeController.java
@@ -1,12 +1,84 @@
 package com.study.SpringBootWeb1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 
+	@RequestMapping("/")
 	public String home() {
+
 		System.out.println("HelloController is called");
-		return "index.jsp";
+		return "index";
+//		return "index.jsp";
 	}
+
+	// remove HttpSession with help of Model Object to get the request object and we
+	// also the HttpSrvletRequest with the help of @RequstParam("the name of
+	// parameter") it's handle the
+	// parameters
+	@RequestMapping("add")
+	// public String add(@RequestParam("num1") int num, @RequestParam("num2") int
+	// nm, Model model)
+	public ModelAndView add(@RequestParam("num1") int num, @RequestParam("num2") int nm, ModelAndView mv) {
+
+		int result = num + nm;
+
+		mv.addObject("result", result);
+		mv.setViewName("result");
+//		model.addAttribute("result", result);
+
+		return mv;
+		// return "result.jsp";
+
+	}
+
+	/*
+	 * @RequestMapping("add") public String add(HttpServletRequest req ,HttpSession
+	 * session) { int num1 = Integer.parseInt(req.getParameter("num1")); int num2 =
+	 * Integer.parseInt(req.getParameter("num2")); int result = num1+num2; sevlet
+	 * way to solve. session.setAttribute("result", result);
+	 * 
+	 * return "result.jsp";
+	 * 
+	 * }
+	 */
+	
+	
+	
+	/*
+	 * @RequestMapping("addAlien") public ModelAndView addAlien(@RequestParam("aid")
+	 * int aid, @RequestParam("aname") String aname, ModelAndView mv) {
+	 * 
+	 * Alien alien = new Alien();
+	 * 
+	 * alien.setAid(aid); alien.setAname(aname);
+	 * 
+	 * mv.addObject("alien", alien); mv.setViewName("result");
+	 * 
+	 * return mv;
+	 * 
+	 * }
+	 */
+	
+	@RequestMapping("addAlien")
+	public String addAlien(@ModelAttribute Alien alien) {
+
+		return "result";
+	
+	}
+	
+	
+	
+	
+	
+	
 }
