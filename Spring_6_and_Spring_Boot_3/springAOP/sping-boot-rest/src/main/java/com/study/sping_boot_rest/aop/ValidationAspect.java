d25871c6710e93e3c3e12bb592cb5e4992585dee
Nikhil-Rathod03 <rathodnikhil130603@gmail.com>
2025-02-28 23:56:39 +0530

    Adding the concept of Spring AOP in sping-boot-rest Logging the Calls,AOP Concepts, Before Advice, JoinPoint, After Advice, Performance Monitoring using Around Advice, Validating the input using Around Advice

---
+++ Spring_6_and_Spring_Boot_3/springAOP/sping-boot-rest/src/main/java/com/study/sping_boot_rest/aop/ValidationAspect.java
@@ -0,0 +1,27 @@
+package com.study.sping_boot_rest.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class ValidationAspect {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(ValidationAspect.class);
+	
+	@Around("execution(* com.study.sping_boot_rest.service.JobService.getJob(..)) && args(postId)")
+	public Object validateAndUpdate(ProceedingJoinPoint jp, int postId) throws Throwable{
+		
+		if(postId < 0) {
+			LOGGER.info("PostId is negative, updateing it");
+		    postId = -postId;
+		    LOGGER.info("new value " + postId);
+		}
+		Object obj = jp.proceed(new Object[]{postId});
+		return obj;
+	}
+}
