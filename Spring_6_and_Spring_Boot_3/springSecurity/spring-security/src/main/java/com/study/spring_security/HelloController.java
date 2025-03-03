7c3db97ba4aeb409771712c88c2644a81c689be8
Nikhil-Rathod03 <rathodnikhil130603@gmail.com>
2025-03-03 19:00:04 +0530

    Adding the concept of Spring Security Creating a Spring Security Project, default login form in local browser write localhost:8080/hello, Session ID, Setting username and password, Basic Auth using Postman, What is CSRF, Error without CSRF Token, Sending CSRF Token, Same Site Strict, Security Configuration, Disabling CSRF Token, without lambda, Getting ready for user database

--- Spring_6_and_Spring_Boot_3/springSecurity/spring-security/src/main/java/com/study/spring_security/HelloController.java
+++ Spring_6_and_Spring_Boot_3/springSecurity/spring-security/src/main/java/com/study/spring_security/HelloController.java
@@ -3,11 +3,18 @@ package com.study.spring_security;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 public class HelloController {
 
 	@GetMapping("/hello")
-	public String greet() {
-		return "Hello Nikhil";
+	public String greet(HttpServletRequest request) {
+		return "Hello Nikhil " + request.getSession().getId();
+	}
+	
+	@GetMapping("/about")
+	public String aboutPage(HttpServletRequest request) {
+		return "Study " + request.getSession().getId();
 	}
 }
